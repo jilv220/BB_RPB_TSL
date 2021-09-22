470cb9136ff8eb900c4cf91f6cd0abc9946f3d66
jilv220 <LyuJi220@hotmail.com>
2021-09-22 17:24:52 -0400

    add strategy and pairlist

---
+++ BB_RPB_TSL.py
@@ -0,0 +1,263 @@
+# --- Do not remove these libs ---
+import freqtrade.vendor.qtpylib.indicators as qtpylib
+import numpy as np
+import talib.abstract as ta
+from freqtrade.persistence import Trade
+from freqtrade.strategy.interface import IStrategy
+from pandas import DataFrame, Series
+from datetime import datetime, timedelta
+from freqtrade.strategy import merge_informative_pair, CategoricalParameter, DecimalParameter, IntParameter, stoploss_from_open
+from functools import reduce
+from technical.indicators import RMI
+
+# --------------------------------
+
+def chaikin_mf(df, periods=20):
+    close = df['close']
+    low = df['low']
+    high = df['high']
+    volume = df['volume']
+    mfv = ((close - low) - (high - close)) / (high - low)
+    mfv = mfv.fillna(0.0)
+    mfv *= volume
+    cmf = mfv.rolling(periods).sum() / volume.rolling(periods).sum()
+    return Series(cmf, name='cmf')
+
+class BB_RPB_TSL(IStrategy):
+    '''
+        BB_RPB_TSL
+        @author jilv220
+
+        Simple bollinger brand strategy inspired by this blog  ( https://hacks-for-life.blogspot.com/2020/12/freqtrade-notes.html )
+        RPB, which stands for Real Pull Pack, taken from ( https://github.com/GeorgeMurAlkh/freqtrade-stuff/blob/main/user_data/strategies/TheRealPullbackV2.py )
+        The trailing custom stoploss taken from BigZ04_TSL from Perkmeister ( modded by ilya )
+
+        I modified it to better suit my taste and added Hyperopt for this strategy.
+
+    '''
+
+    ##########################################################################
+
+    # Hyperopt result area     # buy => roi => sell
+
+    # buy space
+    buy_params = {
+        "buy_bb_delta": 0.012,
+        "buy_bb_factor": 0.99,
+        "buy_bb_width": 0.026,
+        "buy_cci": -91,
+        "buy_cci_length": 26,
+        "buy_rmi": 50,
+        "buy_rmi_length": 9,
+        "buy_srsi_fk": 41,
+    }
+
+    # sell space
+    sell_params = {
+        "pHSL": -0.04,
+        "pPF_1": 0.011,
+        "pPF_2": 0.069,
+        "pSL_1": 0.011,
+        "pSL_2": 0.068,
+    }
+
+    minimal_roi = {
+        "0": 0.052,
+        "28": 0.042,
+        "30": 0.033,
+        "52": 0.027,
+        "79": 0.022,
+        "82": 0.019,
+        "186": 0
+    }
+
+    # Optimal timeframe for the strategy
+    timeframe = '5m'
+    inf_1h = '1h'
+
+    # Disabled
+    stoploss = -0.99
+
+    # Custom stoploss
+    use_custom_stoploss = True
+    use_sell_signal = True
+
+    ############################################################################
+
+    ## Buy params
+
+    buy_rmi = IntParameter(30, 50, default=35)
+    buy_cci = IntParameter(-135, -90, default=-133)
+    buy_srsi_fk = IntParameter(30, 50, default=25)
+
+    buy_bb_width = DecimalParameter(0.022, 0.027, default=0.025)
+    buy_bb_delta = DecimalParameter(0.012, 0.022, default=0.0125)
+    buy_bb_factor = DecimalParameter(0.985, 0.99, default=0.99)
+
+    buy_cci_length = IntParameter(25, 45, default=25)
+    buy_rmi_length = IntParameter(8, 20, default=8)
+
+    # Buy params toggle
+    buy_is_dip_enabled = CategoricalParameter([True, False], default=True, space='buy', optimize=False, load=True)
+    buy_is_break_enabled = CategoricalParameter([True, False], default=True, space='buy', optimize=False, load=True)
+
+    ## Trailing params
+
+    # hard stoploss profit
+    pHSL = DecimalParameter(-0.200, -0.040, default=-0.08, decimals=3, space='sell', load=True)
+    # profit threshold 1, trigger point, SL_1 is used
+    pPF_1 = DecimalParameter(0.008, 0.020, default=0.016, decimals=3, space='sell', load=True)
+    pSL_1 = DecimalParameter(0.008, 0.020, default=0.011, decimals=3, space='sell', load=True)
+
+    # profit threshold 2, SL_2 is used
+    pPF_2 = DecimalParameter(0.040, 0.100, default=0.080, decimals=3, space='sell', load=True)
+    pSL_2 = DecimalParameter(0.020, 0.070, default=0.040, decimals=3, space='sell', load=True)
+
+    ############################################################################
+
+    ## currently not using
+    def informative_pairs(self):
+        pairs = self.dp.current_whitelist()
+        informative_pairs = [(pair, '1h') for pair in pairs]
+        return informative_pairs
+
+    def informative_1h_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        assert self.dp, "DataProvider is required for multiple timeframes."
+
+        ## informative
+
+        # Get the informative pair
+        informative_1h = self.dp.get_pair_dataframe(pair=metadata['pair'], timeframe=self.inf_1h)
+
+        # EMA
+        informative_1h['ema_50'] = ta.EMA(informative_1h, timeperiod=50)
+        informative_1h['ema_200'] = ta.EMA(informative_1h, timeperiod=200)
+        # RSI
+        informative_1h['rsi'] = ta.RSI(informative_1h, timeperiod=14)
+
+        bollinger = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=20, stds=2)
+        informative_1h['bb_lowerband'] = bollinger['lower']
+        informative_1h['bb_middleband'] = bollinger['mid']
+        informative_1h['bb_upperband'] = bollinger['upper']
+
+        return informative_1h
+
+    ############################################################################
+
+    ## Custom Trailing stoploss ( credit to Perkmeister for this custom stoploss to help the strategy ride a green candle )
+    def custom_stoploss(self, pair: str, trade: 'Trade', current_time: datetime,
+                        current_rate: float, current_profit: float, **kwargs) -> float:
+
+        # hard stoploss profit
+        HSL = self.pHSL.value
+        PF_1 = self.pPF_1.value
+        SL_1 = self.pSL_1.value
+        PF_2 = self.pPF_2.value
+        SL_2 = self.pSL_2.value
+
+        # For profits between PF_1 and PF_2 the stoploss (sl_profit) used is linearly interpolated
+        # between the values of SL_1 and SL_2. For all profits above PL_2 the sl_profit value
+        # rises linearly with current profit, for profits below PF_1 the hard stoploss profit is used.
+
+        if (current_profit > PF_2):
+            sl_profit = SL_2 + (current_profit - PF_2)
+        elif (current_profit > PF_1):
+            sl_profit = SL_1 + ((current_profit - PF_1) * (SL_2 - SL_1) / (PF_2 - PF_1))
+        else:
+            sl_profit = HSL
+
+        # For hyperopt only
+        if (sl_profit >= current_profit):
+            return -0.99
+
+        return stoploss_from_open(sl_profit, current_profit)
+
+    ############################################################################
+
+    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+
+        # Bollinger bands (hyperopt hard to implement)
+        bollinger2 = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=20, stds=2)
+        dataframe['bb_lowerband2'] = bollinger2['lower']
+        dataframe['bb_middleband2'] = bollinger2['mid']
+        dataframe['bb_upperband2'] = bollinger2['upper']
+
+        bollinger3 = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=20, stds=3)
+        dataframe['bb_lowerband3'] = bollinger3['lower']
+        dataframe['bb_middleband3'] = bollinger3['mid']
+        dataframe['bb_upperband3'] = bollinger3['upper']
+
+        # The indicators for the 1h informative timeframe
+        #informative_1h = self.informative_1h_indicators(dataframe, metadata)
+        #dataframe = merge_informative_pair(dataframe, informative_1h, self.timeframe, self.inf_1h, ffill=True)
+
+        # Other checks
+        dataframe['bb_width'] = ((dataframe['bb_upperband2'] - dataframe['bb_lowerband2']) / dataframe['bb_middleband2'])
+        dataframe['bb_delta'] = ((dataframe['bb_lowerband2'] - dataframe['bb_lowerband3']) / dataframe['bb_lowerband2'])
+        dataframe['bb_bottom_cross'] = qtpylib.crossed_below(dataframe['close'], dataframe['bb_lowerband3']).astype('int')
+
+        # CCI hyperopt
+        for val in self.buy_cci_length.range:
+            dataframe[f'cci_length_{val}'] = ta.CCI(dataframe, val)
+
+        #dataframe['cci'] = ta.CCI(dataframe, 30)
+
+        # RMI hyperopt
+        for val in self.buy_rmi_length.range:
+            dataframe[f'rmi_length_{val}'] = RMI(dataframe, length=val, mom=4)
+        #dataframe['rmi'] = RMI(dataframe, length=8, mom=4)
+
+        # SRSI hyperopt ?
+        stoch = ta.STOCHRSI(dataframe, 15, 20, 2, 2)
+        dataframe['srsi_fk'] = stoch['fastk']
+        dataframe['srsi_fd'] = stoch['fastd']
+
+        #print(dataframe)
+
+        return dataframe
+
+    def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+
+        conditions = []
+
+        # Hyperopt modules
+        if self.buy_is_dip_enabled.value:
+
+            is_dip = (
+                (dataframe[f'rmi_length_{self.buy_rmi_length.value}'] < self.buy_rmi.value) &
+                (dataframe[f'cci_length_{self.buy_cci_length.value}'] <= self.buy_cci.value) &
+                (dataframe['srsi_fk'] < self.buy_srsi_fk.value)
+            )
+
+            conditions.append(is_dip)
+
+        if self.buy_is_break_enabled.value:
+
+            is_break = (
+
+                (
+                    (dataframe['bb_delta'] > self.buy_bb_delta.value)
+                    |
+                    (dataframe['bb_width'] > self.buy_bb_width.value)
+                )
+                &
+                (dataframe['close'] < self.buy_bb_factor.value * dataframe['bb_lowerband3'])
+            )
+
+            conditions.append(is_break)
+
+        # Check that volume is not 0
+        conditions.append(dataframe['volume'] > 0)
+
+        if conditions:
+            dataframe.loc[ reduce(lambda x, y: x & y, conditions), 'buy' ] = 1
+
+        return dataframe
+
+    def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe.loc[
+            (
+                (dataframe['volume'] > 0) # Make sure Volume is not 0
+            ),
+            'sell'] = 0                                                                      # Disabled
+        return dataframe
