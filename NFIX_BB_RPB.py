baf3632196b11a1a9606e088999fc6397481b155
jilv220 <LyuJi220@hotmail.com>
2021-10-30 14:56:16 -0400

    merge NFIX sell_12 rework

--- NFIX_BB_RPB.py
+++ NFIX_BB_RPB.py
@@ -2502,6 +2502,8 @@ class NFIX_BB_RPB(IStrategy):
                 return True, 'sell_profit_t_1_10'
             elif (max_profit > (current_profit + 0.025)) and (last_candle['rsi_14'] < 42.0):
                 return True, 'sell_profit_t_1_11'
+            elif (max_profit > (current_profit + 0.01)) and (last_candle['rsi_14'] < 44.0) and (last_candle['cmf'] < -0.25):
+                return True, 'sell_profit_t_1_12'
         elif 0.03 > current_profit >= 0.02:
             if (max_profit > (current_profit + 0.015)) and (last_candle['rsi_14'] < 40.0):
                 return True, 'sell_profit_t_2_1'
@@ -2523,6 +2525,8 @@ class NFIX_BB_RPB(IStrategy):
                 return True, 'sell_profit_t_2_10'
             elif (max_profit > (current_profit + 0.03)) and (last_candle['rsi_14'] < 40.0):
                 return True, 'sell_profit_t_2_11'
+            elif (max_profit > (current_profit + 0.025)) and (last_candle['rsi_14'] < 43.0) and (last_candle['cmf'] < -0.25):
+                return True, 'sell_profit_t_2_12'
         elif 0.04 > current_profit >= 0.03:
             if (max_profit > (current_profit + 0.02)) and (last_candle['rsi_14'] < 41.0):
                 return True, 'sell_profit_t_3_1'
@@ -2544,6 +2548,8 @@ class NFIX_BB_RPB(IStrategy):
                 return True, 'sell_profit_t_3_10'
             elif (max_profit > (current_profit + 0.035)) and (last_candle['rsi_14'] < 39.0):
                 return True, 'sell_profit_t_3_11'
+            elif (max_profit > (current_profit + 0.025)) and (last_candle['rsi_14'] < 42.0) and (last_candle['cmf'] < -0.25):
+                return True, 'sell_profit_t_3_12'
         elif 0.05 > current_profit >= 0.04:
             if (max_profit > (current_profit + 0.025)) and (last_candle['rsi_14'] < 42.0):
                 return True, 'sell_profit_t_4_1'
@@ -2565,6 +2571,8 @@ class NFIX_BB_RPB(IStrategy):
                 return True, 'sell_profit_t_4_10'
             elif (max_profit > (current_profit + 0.04)) and (last_candle['rsi_14'] < 38.0):
                 return True, 'sell_profit_t_4_11'
+            elif (max_profit > (current_profit + 0.025)) and (last_candle['rsi_14'] < 41.0) and (last_candle['cmf'] < -0.25):
+                return True, 'sell_profit_t_4_12'
         elif 0.06 > current_profit >= 0.05:
             if (max_profit > (current_profit + 0.03)) and (last_candle['rsi_14'] < 43.0):
                 return True, 'sell_profit_t_5_1'
@@ -2586,6 +2594,8 @@ class NFIX_BB_RPB(IStrategy):
                 return True, 'sell_profit_t_5_10'
             elif (max_profit > (current_profit + 0.045)) and (last_candle['rsi_14'] < 37.0):
                 return True, 'sell_profit_t_5_11'
+            elif (max_profit > (current_profit + 0.03)) and (last_candle['rsi_14'] < 40.0) and (last_candle['cmf'] < -0.25):
+                return True, 'sell_profit_t_5_12'
         elif 0.07 > current_profit >= 0.06:
             if (max_profit > (current_profit + 0.035)) and (last_candle['rsi_14'] < 44.0):
                 return True, 'sell_profit_t_6_1'
@@ -2607,6 +2617,8 @@ class NFIX_BB_RPB(IStrategy):
                 return True, 'sell_profit_t_6_10'
             elif (max_profit > (current_profit + 0.05)) and (last_candle['rsi_14'] < 36.0):
                 return True, 'sell_profit_t_6_11'
+            elif (max_profit > (current_profit + 0.035)) and (last_candle['rsi_14'] < 39.0) and (last_candle['cmf'] < -0.25):
+                return True, 'sell_profit_t_6_12'
         elif 0.08 > current_profit >= 0.07:
             if (max_profit > (current_profit + 0.04)) and (last_candle['rsi_14'] < 43.0):
                 return True, 'sell_profit_t_7_1'
@@ -2628,6 +2640,8 @@ class NFIX_BB_RPB(IStrategy):
                 return True, 'sell_profit_t_7_10'
             elif (max_profit > (current_profit + 0.055)) and (last_candle['rsi_14'] < 36.0):
                 return True, 'sell_profit_t_7_11'
+            elif (max_profit > (current_profit + 0.04)) and (last_candle['rsi_14'] < 38.0) and (last_candle['cmf'] < -0.25):
+                return True, 'sell_profit_t_7_12'
         elif 0.09 > current_profit >= 0.08:
             if (max_profit > (current_profit + 0.045)) and (last_candle['rsi_14'] < 42.0):
                 return True, 'sell_profit_t_8_1'
@@ -2649,6 +2663,8 @@ class NFIX_BB_RPB(IStrategy):
                 return True, 'sell_profit_t_8_10'
             elif (max_profit > (current_profit + 0.06)) and (last_candle['rsi_14'] < 36.0):
                 return True, 'sell_profit_t_8_11'
+            elif (max_profit > (current_profit + 0.045)) and (last_candle['rsi_14'] < 37.0) and (last_candle['cmf'] < -0.25):
+                return True, 'sell_profit_t_8_12'
         elif 0.1 > current_profit >= 0.09:
             if (max_profit > (current_profit + 0.05)) and (last_candle['rsi_14'] < 41.0):
                 return True, 'sell_profit_t_9_1'
@@ -2670,6 +2686,8 @@ class NFIX_BB_RPB(IStrategy):
                 return True, 'sell_profit_t_9_10'
             elif (max_profit > (current_profit + 0.065)) and (last_candle['rsi_14'] < 36.0):
                 return True, 'sell_profit_t_9_11'
+            elif (max_profit > (current_profit + 0.05)) and (last_candle['rsi_14'] < 36.0) and (last_candle['cmf'] < -0.25):
+                return True, 'sell_profit_t_9_12'
         elif 0.12 > current_profit >= 0.1:
             if (max_profit > (current_profit + 0.055)) and (last_candle['rsi_14'] < 40.0):
                 return True, 'sell_profit_t_10_1'
@@ -2691,6 +2709,8 @@ class NFIX_BB_RPB(IStrategy):
                 return True, 'sell_profit_t_10_10'
             elif (max_profit > (current_profit + 0.07)) and (last_candle['rsi_14'] < 35.0):
                 return True, 'sell_profit_t_10_11'
+            elif (max_profit > (current_profit + 0.055)) and (last_candle['rsi_14'] < 35.0) and (last_candle['cmf'] < -0.25):
+                return True, 'sell_profit_t_10_12'
         elif 0.2 > current_profit >= 0.12:
             if (max_profit > (current_profit + 0.06)) and (last_candle['rsi_14'] < 38.0):
                 return True, 'sell_profit_t_11_1'
@@ -2712,6 +2732,8 @@ class NFIX_BB_RPB(IStrategy):
                 return True, 'sell_profit_t_11_10'
             elif (max_profit > (current_profit + 0.075)) and (last_candle['rsi_14'] < 34.0):
                 return True, 'sell_profit_t_11_11'
+            elif (max_profit > (current_profit + 0.06)) and (last_candle['rsi_14'] < 34.0) and (last_candle['cmf'] < -0.25):
+                return True, 'sell_profit_t_11_12'
         elif current_profit >= 0.2:
             if (max_profit > (current_profit + 0.1)) and (last_candle['rsi_14'] < 36.0):
                 return True, 'sell_profit_t_12_1'
@@ -2733,6 +2755,8 @@ class NFIX_BB_RPB(IStrategy):
                 return True, 'sell_profit_t_12_10'
             elif (max_profit > (current_profit + 0.08)) and (last_candle['rsi_14'] < 33.0):
                 return True, 'sell_profit_t_12_11'
+            elif (max_profit > (current_profit + 0.07)) and (last_candle['rsi_14'] < 33.0) and (last_candle['cmf'] < -0.25):
+                return True, 'sell_profit_t_12_12'
 
         return False, None
 
